a08d1d564ee42a63b2859119d5e69c8b1d949e6e
Oybek Kayumov <oybek.kayumov@gmail.com>
2024-06-18 06:31:50 +0500

    Add person setSurname and setAge

--- 1-IoC-DI/src/main/java/com/kayumov/spring/introduction/Test31.java
+++ 1-IoC-DI/src/main/java/com/kayumov/spring/introduction/Test31.java
@@ -11,8 +11,12 @@ public class Test31 {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext.xml");
 
+        //todo_ внедрение с помошью setter, surname -> setSurname
         Person person = context.getBean("myPerson", Person.class);
         person.callYourPet();
+
+        System.out.println(person.getSurname());
+        System.out.println(person.getAge());
 
         context.close();
     }
