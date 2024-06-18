c5528372ef5d1b63347fa691c47b98ecd54609aa
Oybek Kayumov <oybek.kayumov@gmail.com>
2024-06-18 08:26:24 +0500

    Add prototype - create a new stateful bean

--- 1-IoC-DI/src/main/java/com/kayumov/spring/introduction/Test4.java
+++ 1-IoC-DI/src/main/java/com/kayumov/spring/introduction/Test4.java
@@ -12,20 +12,20 @@ public class Test4 {
         Cat myCat = context.getBean("myPet", Cat.class);
         Cat yourCat = context.getBean("myPet", Cat.class);
 
-//        System.out.println("Переменные ссылаются на один и тот же объект? " + (myCat == yourCat));
-        //* true
-//        System.out.println(myCat);
-//        System.out.println(yourCat);
+        System.out.println("Переменные ссылаются на один и тот же объект? " + (myCat == yourCat));
+        //! true - singleton, false - prototype
+        System.out.println(myCat);
+        System.out.println(yourCat);
 
         myCat.setName("Belka");
         yourCat.setName("Strelka");
-
-        System.out.println(myCat.getName());        //* Strelka
-        System.out.println(yourCat.getName());      //* Strelka
-        //! содаётся только 1 объект.
+//todo_                                               singleton     prototype
+        System.out.println(myCat.getName());        //* Strelka     Belka
+        System.out.println(yourCat.getName());      //* Strelka     Strelka
+//        //! содаётся только 1 объект.
 
         context.close();
     }
 }
 
-//Cat bean is created
+//Cat bean is created - singleton ok, prototype - won't display
