cbd3a33bd94e2ea93e97f72de4bff40249e16812
Oybek Kayumov <oybek.kayumov@gmail.com>
2024-07-28 16:55:11 +0500

    Add delete db using hql

--- 1-IoC-DI/src/main/java/com/kayumov/spring/hibernate_base/Test5.java
+++ 1-IoC-DI/src/main/java/com/kayumov/spring/hibernate_base/Test5.java
@@ -4,9 +4,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-import java.util.List;
-
-public class Test4 {
+public class Test5 {
 
     public static void main(String[] args) {
 
@@ -21,13 +19,9 @@ public class Test4 {
             //* open transaction
             session.beginTransaction();
 
-            //* update data in db
-            //Employee employee = session.get(Employee.class, 1);  //* id = 1
-            //employee.setSalary(1500);
-
-            session.createQuery(
-              "update Employee set salary = 1000 where name = 'Alex'")
-                 .executeUpdate();
+            //* delete data from db
+            Employee employee = session.get(Employee.class, 1);  //* id = 1
+            session.delete(employee);
 
             //* close transaction
             session.getTransaction().commit();
