92b3a4e4645cba541fd47310170785192236cc65
Oybek Kayumov <oybek.kayumov@gmail.com>
2024-07-28 07:24:51 +0500

    Add connect to mysql and insert data

--- 1-IoC-DI/src/main/java/com/kayumov/spring/hibernate_base/Test1.java
+++ 1-IoC-DI/src/main/java/com/kayumov/spring/hibernate_base/Test1.java
@@ -1,4 +1,32 @@
 package com.kayumov.spring.hibernate_base;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class Test1 {
+
+    public static void main(String[] args) {
+
+        //* factory
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .buildSessionFactory();
+
+        try {
+            Session session = factory.getCurrentSession();
+
+            //* create an object to save in db
+            Employee employee = new Employee(
+                    "John", "Doe", "IT", 500);
+
+            //* open transaction
+            session.beginTransaction();
+            session.save(employee);             //* insert obj to db
+            session.getTransaction().commit();
+        } finally {
+            factory.close();
+        }
+    }
 }
