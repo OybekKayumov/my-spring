ebd72ee33ff98c9fd0f73661da3780e3a2ec7de8
Oybek Kayumov <oybek.kayumov@gmail.com>
2024-08-04 08:16:33 +0500

    Add many to many

--- 1-IoC-DI/src/main/java/com/kayumov/spring/hibernate_many_to_many/Child.java
+++ 1-IoC-DI/src/main/java/com/kayumov/spring/hibernate_many_to_many/Child.java
@@ -1,9 +1,20 @@
 package com.kayumov.spring.hibernate_many_to_many;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "children")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Child {
 
     @Id
@@ -16,4 +27,22 @@ public class Child {
 
     @Column(name = "age")
     private int age;
+
+    private List<Section> sections;
+
+    @Override
+    public String toString() {
+        return "Child{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public void addSectionToChild(Section section) {
+        if (section == null) {
+            sections = new ArrayList<>();
+        }
+        sections.add(section);
+    }
 }
--- 1-IoC-DI/src/main/java/com/kayumov/spring/hibernate_many_to_many/Section.java
+++ 1-IoC-DI/src/main/java/com/kayumov/spring/hibernate_many_to_many/Section.java
@@ -1,7 +1,18 @@
 package com.kayumov.spring.hibernate_many_to_many;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "section")
 public class Section {
@@ -13,4 +24,22 @@ public class Section {
 
     @Column(name = "name")
     private String name;
+
+    private List<Child> children;
+
+    @Override
+    public String toString() {
+        return "Section{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public void addChildToSection(Child child) {
+        if (child == null) {
+            children = new ArrayList<>();
+        }
+        children.add(child);
+    }
+
 }
