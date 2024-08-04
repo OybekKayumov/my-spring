24f0c9e908ffd4f6ca1a0d5670c50fc51f46d7f9
Oybek Kayumov <oybek.kayumov@gmail.com>
2024-08-04 12:21:44 +0500

    Add many to many

--- 1-IoC-DI/src/main/java/com/kayumov/spring/hibernate_many_to_many/Test1_1.java
+++ 1-IoC-DI/src/main/java/com/kayumov/spring/hibernate_many_to_many/Test1_1.java
@@ -4,7 +4,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class Test1 {
+public class Test1_1 {
 
     public static void main(String[] args) {
 
@@ -19,18 +19,26 @@ public class Test1 {
         try {
             session = factory.getCurrentSession();
 
-            Section section1 = new Section("Dance");
-            Child child1 = new Child("Olya", 12);
-            Child child2 = new Child("Glen", 8);
-            Child child3 = new Child("Veronika", 9);
+            Section section1 = new Section("Football");
+            Child child1 = new Child("John", 5);
+            Child child2 = new Child("Masha", 7);
+            Child child3 = new Child("Vasya", 6);
+
+//            section1.addChildToSection(child1);
+//            section1.addChildToSection(child2);
+//            section1.addChildToSection(child3);
+
+            session.beginTransaction();
+
+            session.save(section1);
 
             section1.addChildToSection(child1);
             section1.addChildToSection(child2);
             section1.addChildToSection(child3);
 
-            session.beginTransaction();
-
-            session.persist(section1);
+            session.save(child1);
+            session.save(child2);
+            session.save(child3);
 
             session.getTransaction().commit();
             System.out.println("Done!");
