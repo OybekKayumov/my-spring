46672d2082374b7219657292e0b9e399b02e4c8e
Oybek Kayumov <oybek.kayumov@gmail.com>
2024-08-04 12:17:43 +0500

    Add many to many

--- 1-IoC-DI/src/main/java/com/kayumov/spring/hibernate_many_to_many/Child.java
+++ 1-IoC-DI/src/main/java/com/kayumov/spring/hibernate_many_to_many/Child.java
@@ -28,7 +28,9 @@ public class Child {
     @Column(name = "age")
     private int age;
 
-    @ManyToMany(cascade = CascadeType.ALL)
+    //@ManyToMany(cascade = CascadeType.ALL)
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE,
+            CascadeType.REFRESH, CascadeType.DETACH})
     @JoinTable(
             name = "child_section",
             joinColumns = @JoinColumn(name = "child_id"),
--- 1-IoC-DI/src/main/java/com/kayumov/spring/hibernate_many_to_many/Test5.java
+++ 1-IoC-DI/src/main/java/com/kayumov/spring/hibernate_many_to_many/Test5.java
@@ -4,7 +4,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class Test1 {
+public class Test5 {
 
     public static void main(String[] args) {
 
@@ -18,27 +18,11 @@ public class Test1 {
 
         try {
             session = factory.getCurrentSession();
-
-            Section section1 = new Section("Football");
-            Child child1 = new Child("John", 5);
-            Child child2 = new Child("Masha", 7);
-            Child child3 = new Child("Vasya", 6);
-
-//            section1.addChildToSection(child1);
-//            section1.addChildToSection(child2);
-//            section1.addChildToSection(child3);
-
             session.beginTransaction();
 
-            session.save(section1);
+            Section section = session.get(Section.class, 1);
 
-            section1.addChildToSection(child1);
-            section1.addChildToSection(child2);
-            section1.addChildToSection(child3);
-
-            session.save(child1);
-            session.save(child2);
-            session.save(child3);
+            session.delete(section);
 
             session.getTransaction().commit();
             System.out.println("Done!");
